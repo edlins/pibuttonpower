51603f5d07f94e428e0d40fe13c6e057e383320e
Scott Edlin <edlins@yahoo.com>
2018-05-20 12:43:43 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cp pibuttonpower /usr/sbin
+cp pibuttonpower /usr/local/bin
 cp init.d/pibuttonpower /etc/init.d
 chmod 755 /etc/init.d/pibuttonpower
 update-rc.d pibuttonpower defaults

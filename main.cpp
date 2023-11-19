474c10bcae53c742ebd582448be1f19d385c200e
scottomatic <scotte@touchsource.com>
2023-11-19 14:37:16 -0700

    ISSUE-3 cleanup

--- main.cpp
+++ main.cpp
@@ -17,15 +17,10 @@ License: Freeware
 #include <syslog.h>
 #include <string.h>
 #include <signal.h>
-// We want a success/failure return value from 'wiringPiSetup()'
-//#define WIRINGPI_CODES 1
-// #include <wiringPi.h>
 #include <pigpio.h>
 
 #define DAEMON_NAME "pibuttonpower"
 #define PID_FILE "/var/run/" DAEMON_NAME ".pid"
-// #define PIN			26	/* This is wiringPi pin 0 which is physical pin 8 */
-// #define PIN_STR			"26"
 char *pinstr;
 int pinnum;
 
@@ -36,7 +31,6 @@ void Button_Pressed(int gpio, int level, uint32_t tick);
 /* ------------------ Start Here ----------------------- */
 int main(int argc, char *argv[])
 {
-   /* command line arg */
    int c;
    while ((c = getopt(argc, argv, "p:")) != -1)
    {
@@ -53,19 +47,18 @@ int main(int argc, char *argv[])
          return 1;
       default:
          abort();
-      } // switch
-   }    // while
+      }
+   }
    if (pinstr == NULL)
    {
       fprintf(stderr, "Error: pin must be specified with -p <wiringPiPin>\n");
       return 1;
-   } // unless
+   }
    if ((pinnum < 1) || (pinnum > 31))
    {
       fprintf(stderr, "Error: pin %d is not between 1 and 20\n", pinnum);
       return 1;
    }
-   // printf("Starting daemon on pin %d\n", pinnum);
 
    /* Logging */
    setlogmask(LOG_UPTO(LOG_INFO));
@@ -79,14 +72,6 @@ int main(int argc, char *argv[])
       exit(EXIT_FAILURE);
    }
 
-   /* Make sure the file '/usr/local/bin/gpio' exists */
-   //	struct stat filestat;
-   //	if (stat("/usr/local/bin/gpio", &filestat) == -1)
-   //	{
-   //	    syslog(LOG_ERR, "The program '/usr/local/bin/gpio' is missing, exiting");
-   //	    exit(EXIT_FAILURE);
-   //	}
-
    /* Ensure only one copy */
    /*
    Common user should be able to read the pid file so that they
@@ -95,7 +80,6 @@ int main(int argc, char *argv[])
       1. Read & Write permission for owner
       2. Read permission for group and others
    */
-   syslog(LOG_INFO, "Handle PID file\n");
    const int PIDFILE_PERMISSION = 0644;
    int pidFilehandle = open(PID_FILE, O_RDWR | O_CREAT, PIDFILE_PERMISSION);
    if (pidFilehandle == -1)
@@ -126,7 +110,6 @@ int main(int argc, char *argv[])
    if (pid > 0)
       exit(EXIT_SUCCESS);
 
-   syslog(LOG_INFO, "Get and format PID\n");
    /* Get and format PID */
    char szPID[16];
    sprintf(szPID, "%d\n", getpid()); // Call 'getpid()', don't use 'pid' variable
@@ -138,23 +121,19 @@ int main(int argc, char *argv[])
    umask(0);
 
    /* Create a new SID for the child process */
-   syslog(LOG_INFO, "setsid()\n");
    sid = setsid();
    if (sid < 0) /* Log the failure */
    {
       syslog(LOG_ERR, "setsid() failed with %d\n", sid);
       exit(EXIT_FAILURE);
    }
-   syslog(LOG_INFO, "setsid() succeeded with %d\n", sid);
 
    /* Change the current working directory */
-   syslog(LOG_INFO, "chdir()\n");
    if (chdir("/") < 0) /* Log the failure */
    {
       syslog(LOG_ERR, "chdir() failed with a negative value");
       exit(EXIT_FAILURE);
    }
-   syslog(LOG_INFO, "chdir() succeeded\n");
 
    /* Close out the standard file descriptors */
    close(STDIN_FILENO);
@@ -162,12 +141,10 @@ int main(int argc, char *argv[])
    close(STDERR_FILENO);
 
    /* Daemon-specific initializations */
-   syslog(LOG_INFO, "Register SIGTERM handler\n");
    /* Add a process termination handler for
       handling daemon stop requests */
    signal(SIGTERM, &Daemon_Stop);
 
-   syslog(LOG_INFO, "Initializing pigpio\n");
    int init = gpioInitialise();
    if (init < 0)
    {
@@ -197,17 +174,13 @@ The Big Loop
  1. When pressed for less than 2 secs, shutdown system
  2. When pressed for 2 secs or more, restart system
 */
-
-   syslog(LOG_INFO, "Start the big loop\n");
    while (true)
    {
       /*
       Daemon hearbeat
          Just wait until there's an interrupt or system shutdown
       */
-      syslog(LOG_INFO, "sleep\n");
       sleep(60);
-      syslog(LOG_INFO, "wake\n");
    }
 }
 
@@ -221,43 +194,28 @@ void Daemon_Stop(int signum)
 void Button_Pressed(int gpio, int level, uint32_t tick)
 {
    /* Handle button pressed interrupts */
-
    /* Disable further interrupts */
-   /* NOTE: Unfortunately, 'wiringPi' library doesn't support
-      unhooking an existing interrupt handler, so we need
-      to use 'gpio' binary to do this according to the author */
-   //char command[128];
-   //strcpy(command, "/usr/local/bin/gpio edge ");
-   //strcat(command, pinstr);
-   //strcat(command, " none");
-   //syslog(LOG_INFO, "command = %s\n", command);
-   // syslog(LOG_INFO, command);
-   //	system(command);
    gpioSetAlertFunc(pinnum, NULL);
 
-   /* Just wait for user to press the button */
+   /* Just wait for user to release the button */
    sleep(2);
 
-   //	switch (digitalRead(pinnum))
-   switch (gpioRead(pinnum)) {
-      case PI_HIGH:
-         syslog(LOG_INFO, "Shutting down system");
-         break;
-   //		    if (execl("/sbin/poweroff", "poweroff", NULL) == -1)
-   //			syslog(LOG_ERR, "'poweroff' program failed to run with error: %d", errno);
-   // NOTE: Execution will not reach here if 'execl()' succeeds
-          //break;
+   switch (gpioRead(pinnum))
+   {
+   case PI_HIGH:
+      syslog(LOG_INFO, "Shutting down system");
+      if (execl("/sbin/poweroff", "poweroff", NULL) == -1)
+         syslog(LOG_ERR, "'poweroff' program failed to run with error: %d", errno);
+      // NOTE: Execution will not reach here if 'execl()' succeeds
+      break;
 
-   //		case LOW:	// Restart requested
-      case PI_LOW:
-         syslog(LOG_INFO, "Restarting system");
-         break;
-
-   //		    if (execl("/sbin/shutdown", "shutdown", "-r", "now", NULL) == -1)
-   //			syslog(LOG_ERR, "'shutdown' program failed to run with error: %d", errno);
-
-   // NOTE: Execution will not reach here if 'execl()' succeeds
+   case PI_LOW:
+      syslog(LOG_INFO, "Restarting system");
+      if (execl("/sbin/shutdown", "shutdown", "-r", "now", NULL) == -1)
+         syslog(LOG_ERR, "'shutdown' program failed to run with error: %d", errno);
+      // NOTE: Execution will not reach here if 'execl()' succeeds
+      break;
    }
 
-   //exit(EXIT_SUCCESS);
+   exit(EXIT_SUCCESS);
 }
